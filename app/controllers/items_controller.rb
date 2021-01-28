0991b0251402f74d06769d94afe54175a9ab0029
morodomimasaki <morokun0926@gmail.com>
2021-01-28 14:11:35 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :uretara_hennsyuudekinai, only: [:edit]
+
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -22,6 +25,16 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   # def destroy
@@ -32,7 +45,10 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :product_name, :product_description, :category_id, :product_condition_id,
-                                 :product_condition_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, :selling_price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :product_name, :product_description, :category_id, :product_condition_id,:product_condition_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, :selling_price).merge(user_id: current_user.id)
+  end
+
+  def uretara_hennsyuudekinai
+    redirect_to root_path unless current_user
   end
 end
