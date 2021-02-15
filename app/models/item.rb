3ca068ac816f05e9fd5f5c02057ff270225f3243
morodomimasaki <morokun0926@gmail.com>
2021-02-16 00:27:30 +0900

    削除機能追加　不要なもの消去

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
-  has_many :messages
+  has_many :messages,  dependent: :destroy
   has_many_attached :images
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
